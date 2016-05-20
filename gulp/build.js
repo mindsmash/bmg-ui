1c8ed90b71f1711fd570207d38b25ad0dc3b20ba
Sascha Zuske <sascha.zuske@mindsmash.com>
2016-05-20 10:38:56 +0200

    0.1.8 before git db issues

---
+++ gulp/build.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var gulpSequence = require('gulp-sequence');
+
+// ----------------------------------------------------------------------------------------------------
+
+gulp.task('default', ['build']); // default task
+
+/**
+ * Main build task that procudes the 'dist' folder.
+ */
+gulp.task('build', gulpSequence(
+  'styles:build',
+  'scripts:build',
+  'inject:build',
+  'copy:fonts',
+  'copy:images'
+));
