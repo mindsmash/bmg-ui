140ee6bc2cf2d0c9899de88f4c4d033452e02572
gannimet <r.wotzlaw@gmail.com>
2016-06-30 17:12:51 +0200

    expand hint in collapsed navbar

--- source/kit/components/ui/collapsingNavbar.js
+++ source/kit/components/ui/collapsingNavbar.js
@@ -8,44 +8,53 @@
     function collapsingNavbar() {
         return {
             link: function(scope, elem) {
-                // saves the previous 10 scroll positions
-                var lastKnownScrollPositions = [];
+                window.setInterval(checkScrollStatus, 200);
 
-                window.setInterval(function() {
-                    // check scroll status every 200ms
-                    var scrollTop = $(document).scrollTop();
-                    lastKnownScrollPositions.push(scrollTop);
+                $('nav.navbar').click(expandNavbar);
 
-                    if (lastKnownScrollPositions.length > 10) {
-                        // only the last 10 positions should be saved
-                        lastKnownScrollPositions.shift();
+                // add expand hint
+                var expandHint = angular.element(
+                    '<div class="bmg-nav-expand-hint"><i class="fa fa-bars"></i></div>');
 
-                        var earliestKnownScrollTop = lastKnownScrollPositions[0];
-                        if ((scrollTop - earliestKnownScrollTop) > 200) {
-                            // more than 200px scrolled down? -> collapse
-                            collapseNavbar();
-                        }
-
-                        if ((earliestKnownScrollTop - scrollTop) > 200 || scrollTop < 30) {
-                            // at the top of the page or more than 200px
-                            // scrolled up? -> expand
-                            expandNavbar();
-                        }
-                    }
-                }, 200);
-
-                $('nav.navbar').click(function() {
-                    expandNavbar();
-                });
-
-                function collapseNavbar() {
-                    $('nav.navbar').addClass('smaller');
-                }
-
-                function expandNavbar() {
-                    $('nav.navbar').removeClass('smaller');
-                }
+                elem.find('.container-fluid').append(expandHint);
             }
         };
+    }
+
+    // saves the previous 10 scroll positions
+    var lastKnownScrollPositions = [];
+
+    function checkScrollStatus() {
+        // check scroll status every 200ms
+        var scrollTop = $(document).scrollTop();
+        lastKnownScrollPositions.push(scrollTop);
+
+        if (lastKnownScrollPositions.length > 10) {
+            // only the last 10 positions should be saved
+            lastKnownScrollPositions.shift();
+
+            var earliestKnownScrollTop = lastKnownScrollPositions[0];
+            var previousScrollTop = lastKnownScrollPositions[lastKnownScrollPositions.length - 2];
+
+            if ((scrollTop - earliestKnownScrollTop) > 200) {
+                // more than 200px scrolled down? -> collapse
+                collapseNavbar();
+            }
+
+            if ((earliestKnownScrollTop - scrollTop) > 200 ||
+                (scrollTop <= 50 && previousScrollTop !== scrollTop)) {
+                // at the top of the page or more than 200px
+                // scrolled up? -> expand
+                expandNavbar();
+            }
+        }
+    }
+
+    function collapseNavbar() {
+        $('nav.navbar').addClass('smaller');
+    }
+
+    function expandNavbar() {
+        $('nav.navbar').removeClass('smaller');
     }
 })();
