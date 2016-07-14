5999ad11740509b2a74e60ca0fbc182cf4e4f7be
gannimet <r.wotzlaw@gmail.com>
2016-07-14 10:52:26 +0200

    moved bmg datepicker template code to same location as typeahead template

--- source/kit/components/util/datepicker-tpl-overrides.js
+++ source/kit/components/util/datepicker-tpl-overrides.js
@@ -118,32 +118,4 @@
             "</div>\n" +
             "");
     }]);
-
-    angular.module('bmg/template/datepicker/control.html', []).run(['$templateCache', function($templateCache) {
-        $templateCache.put('bmg/template/datepicker/control.html',
-        '<p' +
-        '    class="input-group datepicker-group">' +
-        '    <input' +
-        '        type="text"' +
-        '        class="form-control"' +
-        '        uib-datepicker-popup="{{ uibDatepickerPopup }}"' +
-        '        data-ng-model="selectedDate.value"' +
-        '        data-ng-model-options="modelOptions"' +
-        '        data-ng-change="updateDate()"' +
-        '        is-open="bmgDatepickerCtrl.popup.opened"' +
-        '        datepicker-options="datepickerOptions"' +
-        '        data-ng-required="{{ required }}"' +
-        '        close-text="{{ closeText }}"' +
-        '        placeholder="{{ placeholder }}"' +
-        '        popup-placement="{{ popupPlacement }}" />' +
-        '    <span class="input-group-btn">' +
-        '        <button' +
-        '            type="button"' +
-        '            class="btn btn-default shrink"' +
-        '            ng-click="bmgDatepickerCtrl.open()">' +
-        '            <i class="color-primary fa fa-calendar"></i>' +
-        '        </button>' +
-        '    </span>' +
-        '</p>');
-    }]);
 })(angular);
