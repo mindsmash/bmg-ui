2f9bf78633f1550bb269c4e9a8a0f4d33db1aa04
Sascha Zuske <sascha.zuske@mindsmash.com>
2017-01-06 15:35:01 +0100

    v0.9.0

--- docs/scripts/docs.js
+++ docs/scripts/docs.js
@@ -698,6 +698,60 @@
 
 })(angular);
 
+(function(undefined) {
+    'use strict';
+
+    angular
+        .module('bmg-ui.docs')
+        .controller('LoadingController', LoadingController)
+        .directive('contentPlaceholderReloadButton', contentPlaceholderReloadButton);
+
+    LoadingController.$inject = ['$q', '$timeout'];
+
+    function LoadingController($q, $timeout) {
+        var vm = this;
+
+        vm.loadContent = loadContent;
+
+        function loadContent() {
+            var deferred = $q.defer();
+
+            $timeout(function() {
+                vm.content = {
+                    heading: 'Some exciting headline',
+                    text: 'An even more exciting content.'
+                };
+
+                deferred.resolve();
+            }, 2000);
+
+            return deferred.promise;
+        }
+    }
+
+    contentPlaceholderReloadButton.$inject = ['$compile'];
+
+    function contentPlaceholderReloadButton($compile) {
+        return {
+            restrict: 'A',
+            link: function(scope, elem, attrs) {
+                $(elem).click(function() {
+                    var newElemHtml = '<data-content-placeholder' +
+    					'  id="j4825673gr2354f456f3"' +
+    					'  data-content-promise="ctrl.loadContent()">' +
+    					'  <h2 data-ng-bind="ctrl.content.heading"></h2>' +
+    					'  <p data-ng-bind="ctrl.content.text"></p>' +
+    				'</data-content-placeholder>';
+                    var newElem = angular.element(newElemHtml);
+
+                    $('#j4825673gr2354f456f3').replaceWith(newElem);
+                    $compile(newElem)(scope);
+                });
+            }
+        };
+    }
+})();
+
 (function(angular) {
     'use strict';
 
@@ -765,60 +819,6 @@
     }
 
 })(angular);
-
-(function(undefined) {
-    'use strict';
-
-    angular
-        .module('bmg-ui.docs')
-        .controller('LoadingController', LoadingController)
-        .directive('contentPlaceholderReloadButton', contentPlaceholderReloadButton);
-
-    LoadingController.$inject = ['$q', '$timeout'];
-
-    function LoadingController($q, $timeout) {
-        var vm = this;
-
-        vm.loadContent = loadContent;
-
-        function loadContent() {
-            var deferred = $q.defer();
-
-            $timeout(function() {
-                vm.content = {
-                    heading: 'Some exciting headline',
-                    text: 'An even more exciting content.'
-                };
-
-                deferred.resolve();
-            }, 2000);
-
-            return deferred.promise;
-        }
-    }
-
-    contentPlaceholderReloadButton.$inject = ['$compile'];
-
-    function contentPlaceholderReloadButton($compile) {
-        return {
-            restrict: 'A',
-            link: function(scope, elem, attrs) {
-                $(elem).click(function() {
-                    var newElemHtml = '<data-content-placeholder' +
-    					'  id="j4825673gr2354f456f3"' +
-    					'  data-content-promise="ctrl.loadContent()">' +
-    					'  <h2 data-ng-bind="ctrl.content.heading"></h2>' +
-    					'  <p data-ng-bind="ctrl.content.text"></p>' +
-    				'</data-content-placeholder>';
-                    var newElem = angular.element(newElemHtml);
-
-                    $('#j4825673gr2354f456f3').replaceWith(newElem);
-                    $compile(newElem)(scope);
-                });
-            }
-        };
-    }
-})();
 
 (function(angular) {
     'use strict';
